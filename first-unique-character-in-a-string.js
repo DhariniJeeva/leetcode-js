865241e73029fa597f8fe00ea06a968e436e4591
DJ <DJ@Dharinis-MacBook-Pro-3.local>
2018-04-28 20:31:59 -0400

    adding file 1

---
+++ first-unique-character-in-a-string.js
@@ -0,0 +1,9 @@
+var firstUniqueCharacter = function(s){
+    if(s.indexOf(s[i]) ===s.lastIndexOf(s[i]]){
+      return i;
+    }
+
+    return -1;
+}
+
+console.log(firstUniqueCharacter('leetcodelovecode'));
